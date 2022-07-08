b5fb50769af2096ce1745c0dac1f9b1e5abe3365
Vladyslav <prostoandrei9@gmail.com>
2022-07-09 00:51:53 +0300

    create cards

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,12 +1,15 @@
 import style from './shopItem.module.scss';
 
-function ShopItem({imageUrl, title, price, id}){
-    return(
-        <div className={style.shopItem}>
-            <img src={imageUrl} alt={`${title.toUpperCase()}_image`} />
-            <p>{title}</p>
-        </div>
-    )
+function ShopItem({ imageUrl, title, price, id }) {
+  return (
+    <div className={style.shopItem}>
+      <img src={imageUrl} alt={`${title.toUpperCase()}_image`} />
+      <div className={style.description}>
+        <p>{title}</p>
+        <button>add to cart</button>
+      </div>
+    </div>
+  );
 }
 
-export default ShopItem
+export default ShopItem;
